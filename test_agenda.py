744900bfb5dcd3e607b7ea769aa478582ea18116
Sophie Landver <sophielandver@Sophies-MacBook-Pro.local>
2016-03-10 19:27:46 -0800

    changes

--- test_agenda.py
+++ test_agenda.py
@@ -205,29 +205,7 @@ def test_agenda2():
     expected_agenda = Agenda.from_file(io.StringIO(expected_txt))
     assert busy_agenda.complement(freeblock) == expected_agenda
     
-    #TEST complementTimeSpan function
-    busy_agtxt = """
-    02/22/2016 8:00 AM-02/22/2016 9:00 AM|busy
-    02/22/2016 10:00 AM-02/22/2016 12:00 PM|busy
-    02/22/2016 1:00 PM-02/22/2016 2:00 PM|busy
-    02/22/2016 3:00 PM-02/22/2016 5:00 PM|busy
-    02/22/2016 6:00 PM-02/22/2016 7:00 PM|busy
-    """
-    busy_agenda = Agenda.from_file(io.StringIO(busy_agtxt))
-    
-    begin_date = arrow.get("12:00 PM 02/22/2016","h:mm A MM/DD/YYYY") #from 22nd
-    end_date = arrow.get("12:00 PM 02/23/2016","h:mm A MM/DD/YYYY") #to 23rd
-    begin_time = arrow.get("10:00 AM 02/22/2016","h:mm A MM/DD/YYYY") #begin time is 10 AM
-    end_time = arrow.get("4:00 PM 02/22/2016","h:mm A MM/DD/YYYY") #end time is 4 PM
-    
-    expected2_txt = """
-    02/22/2016 12:00 PM-02/22/2016 1:00 PM|Available
-    02/22/2016 2:00 PM-02/22/2016 3:00 PM|Available
-    02/23/2016 10:00 AM-02/23/2016 4:00 PM|Available
-    """
-    expected2_agenda = Agenda.from_file(io.StringIO(expected2_txt))
-    assert busy_agenda.complementTimeSpan(begin_date, end_date, begin_time, end_time) == expected2_agenda
-    
+   
     
 
 
